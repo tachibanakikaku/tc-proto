b1b65d61ecb65e5b2171f7d3b08a34d47c73111b
mryoshio <yoshiokaas@gmail.com>
2016-07-08 16:44:09 +0900

    build basic app

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-from flask import Flask
-app = Flask(__name__)
-app.config['DEBUG'] = True
+import webapp2
+from datetime import datetime
+from google.appengine.ext import ndb
 
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
+class AccessLog(ndb.Model):
+    accessed_at = ndb.DateTimeProperty(auto_now_add=True)
 
+    @classmethod
+    def newest(cls):
+        return cls.query().order(-cls.accessed_at).fetch(1)
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello Google App Engine World!')
 
+class DbPage(webapp2.RequestHandler):
+    def get(self):
+        cnt = AccessLog.query().count()
+        if cnt == 0:
+            self.response.write('no data')
+        else:
+            newest = AccessLog.newest()
+            self.response.write("count {}".format(cnt))
+            self.response.write('<br />')
+            self.response.write(newest)
+    def post(self):
+        AccessLog().put()
 
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/db', DbPage),
+], debug=True)
